4977d072a0c5cd20128d0fe8a4489317535a2a25
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-18 16:25:07 +0800

    poup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const navBar = () => {
       <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[beige] w-[400px] px-[20px] py-[30px] rounded-[10px] shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)] border-solid border-[1px] border-black'>
         <div className='absolute top-[10px] right-[20px] cursor-pointer w-[10px] h-[10px] text-gray-800 text-center font-semibold text-[20px]'>&times;</div>
         <div className='form'>
-          <h2 className='text-center mx-[20px] my-[10px] text-[22px] font-extrabold'>Welcome to</h2>
+          <h2 className='text-center mx-[20px] my-[10px] text-[22px] font-extrabold drop-shadow-xl'>Welcome to</h2>
           <div className='mt-[10px]'>
             <label form='email' className='text-[16px] font-medium'>Email</label>
             <input className= 'block mt-[5px] w-[100%] p-[10px] outline-none border-solid-[1px] rounded-[5px]' type='text' id='email' placeholder='Enter email'></input>
