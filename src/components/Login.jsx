354cf19370de41cf70e6f7726c4abf127d2e96ee
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-21 20:52:31 +0800

    re-making login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const Login = ({open}) => {
+const Login = ({open, closeLogin}) => {
     if(!open) return null
   return (
-    <div>Login</div>
+    <div className='overlay'>
+    <div className='loginContainer'>
+        <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[beige] w-[400px] px-[20px] py-[30px] rounded-[10px] shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)] border-solid border-[1px] border-black'>
+            <div on onClick={closeLogin} className='absolute top-[10px] right-[20px] cursor-pointer w-[10px] h-[10px] text-gray-800 text-center font-semibold text-[20px]'>&times;</div>
+                <div className='form'>
+          <h2 className='text-center mx-[20px] my-[10px] text-[22px] font-extrabold drop-shadow-xl'>Welcome to</h2>
+            <div className='mt-[10px]'>
+                <label form='email' className='text-[16px] font-medium'>Email</label>
+                <input className= 'block mt-[5px] w-[100%] p-[10px] outline-none border-solid-[1px] rounded-[5px]' type='text' id='email' placeholder='Enter email'></input>
+            </div>
+            <div className='mt-[10px]'>
+                <label form='password'className='text-[16px] font-medium'>Password</label>
+                <input className= 'block mt-[5px] w-[100%] p-[10px] outline-none border-solid-[1px] rounded-[5px]' type='password' id='password' placeholder='Enter password'></input>
+            </div>
+            <div className='mt-[10px]'>
+                <input className='mr-[10px]' type='checkbox' id='remember-me' placeholder='Enter password'></input>
+                <label form='remember-me' className='text-[16px] font-medium'>Remember me</label>
+            </div>
+            <div className='mt-[10px]'>
+                <button className='font-bold w-[150px] h-[30px] boreder-none outline-none text-[16px] bg-orange-600 text-white rounded-full'>Sign in</button>
+            </div>
+            <div className='mt-[10px]'>
+                <a href='#'>Forgot password?</a>
+            </div>
+        </div>
+         </div>
+        </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
 import { AiOutlineClose } from "react-icons/ai";
 import { TiThMenu } from "react-icons/ti";
-import {login} from "./Login.jsx"
 
 import logo1 from '../assets/Icon1.png';
 
 const navBar = () => {
     const [nav, setNav] = useState(false);
-    const [login, popLogin] = useState(false);
 
   return (
     <div className ='header'>
@@ -19,7 +17,9 @@ const navBar = () => {
         <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><a href='#'>Testimonials</a></li>
       </ul>
       <ul className='hidden md:flex'>
-        <li onClick={() => popLogin(!login)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><a href='#'>Log In</a></li>
+        <li><div>
+          <button className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'>Log In</button>
+        </div></li>
         <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><a href='#' className='text-white bg-[#1D5B79] rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</a></li>
       </ul>
       <div onClick={() => setNav(!nav)} className='block md:hidden'>
@@ -28,8 +28,7 @@ const navBar = () => {
       <div className={nav ? 'fixed left-0 top-0 w-[300px] h-full border-l-solid border-l-[15px] border-l-[#1D5B79] bg-white ease-in-out duration-500' : 'fixed left-[-100%]'}>
       <img className='h-[70px] m-[10px] pt-[9px]' src={logo1}></img>
         <ul>
-          <li><button className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#' className='active2'>About us</a></button></li>
-          <Login open={login} />
+          <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'>About us</li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Careers</a></li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Services</a></li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Testimonials</a></li>
@@ -38,30 +37,6 @@ const navBar = () => {
           <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#1D5B79]'><a href='#'>Log In</a></li>
           <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><a href='#' className='text-white bg-orange-600 rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</a></li>
         </ul>
-      </div>
-      <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[beige] w-[400px] px-[20px] py-[30px] rounded-[10px] shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)] border-solid border-[1px] border-black'>
-        <div className='absolute top-[10px] right-[20px] cursor-pointer w-[10px] h-[10px] text-gray-800 text-center font-semibold text-[20px]'>&times;</div>
-        <div className='form'>
-          <h2 className='text-center mx-[20px] my-[10px] text-[22px] font-extrabold drop-shadow-xl'>Welcome to</h2>
-          <div className='mt-[10px]'>
-            <label form='email' className='text-[16px] font-medium'>Email</label>
-            <input className= 'block mt-[5px] w-[100%] p-[10px] outline-none border-solid-[1px] rounded-[5px]' type='text' id='email' placeholder='Enter email'></input>
-          </div>
-          <div className='mt-[10px]'>
-            <label form='password'className='text-[16px] font-medium'>Password</label>
-            <input className= 'block mt-[5px] w-[100%] p-[10px] outline-none border-solid-[1px] rounded-[5px]' type='password' id='password' placeholder='Enter password'></input>
-          </div>
-          <div className='mt-[10px]'>
-            <input className='mr-[10px]' type='checkbox' id='remember-me' placeholder='Enter password'></input>
-            <label form='remember-me' className='text-[16px] font-medium'>Remember me</label>
-          </div>
-          <div className='mt-[10px]'>
-            <button className='font-bold w-[150px] h-[30px] boreder-none outline-none text-[16px] bg-orange-600 text-white rounded-full'>Sign in</button>
-          </div>
-          <div className='mt-[10px]'>
-            <a href='#'>Forgot password?</a>
-          </div>
-        </div>
       </div>
     </div>
   )
