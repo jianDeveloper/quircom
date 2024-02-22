1377599e7bdd167269cdb4dc0a371cfc2d69aba4
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-23 01:49:58 +0800

    blurr added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Login = ({open, onClose}) => {
     if(!open) return null
   return (
-    <div className='overlay'>
+    <div className='fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm ease-in-out duration-1000'>
         
     <div className='loginContainer'>
         <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-[beige] w-[400px] px-[20px] py-[30px] rounded-[10px] shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)] border-solid border-[1px] border-black'>
-            <div on onClick={onClose} className='absolute top-[10px] right-[20px] cursor-pointer w-[10px] h-[10px] text-gray-800 text-center font-semibold text-[20px]'>&times;</div>
+            <div on onClick={onClose} className='absolute top-[10px] right-[20px] cursor-pointer w-[10px] h-[10px] text-gray-800 text-center font-semibold text-[20px] ease-in-out duration-1000'>&times;</div>
                 <div className='form'>
           <h2 className='text-center mx-[20px] my-[10px] text-[22px] font-extrabold drop-shadow-xl'>Welcome to</h2>
             <div className='mt-[10px]'>
