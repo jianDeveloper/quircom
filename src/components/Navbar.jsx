b4af48df90babeab56eaa6feaa66ab07427fe008
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-25 10:57:41 +0800

    login done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,17 +27,18 @@ const navBar = () => {
         {!nav ? <TiThMenu size={22}/> : <AiOutlineClose size={22}/> }
       </div>
       <div className={nav ? 'fixed md:hidden left-0 top-0 w-[300px] h-full border-l-solid border-l-[15px] border-l-[#1D5B79] bg-white ease-in-out duration-500' : 'fixed left-[-100%]'}>
-      <img className='h-[70px] m-[10px] pt-[9px]' src={logo1}></img>
+      <img className='h-[70px] m-[10px] pt-[9px]' src={logo1} />
         <ul>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'>About us</li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Careers</a></li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Services</a></li>
           <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Testimonials</a></li>
         </ul>
-        <ul className='mt-[50px]'>
-          <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#1D5B79]'><a href='#'>Log In</a></li>
-          <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><a href='#' className='text-white bg-orange-600 rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</a></li>
-        </ul>
+        <div className='flex-inline justify-center items-center mt-[50px]'>
+          <button onClick={() => setLogin(true)} className='flex mx-auto pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#1D5B79]'>Log In</button>
+          <Modal open={openLogin} onClose={() => setLogin(false)}/>
+          <button className='flex mx-auto pt-2 pb-2 pl-3 pr-3 text-center font-bold'><a href='#' className='text-white bg-orange-600 rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</a></button>
+        </div>
       </div>
     </div>
   )
