a1be0599c797078b6eed2ce06346cb1f1c4975e6
vinsu <vencepuno.work@gmail.com>
2024-02-26 13:15:13 +0800

    Update Hero
    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,19 @@ import Mascot from '../assets/mascot.png'
 const Hero = () => {
   return (
    
-      <div className="max-w-[1240px] h-screen mx-auto flex flex-col mt-[20px] text-center justify-center">
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold py-3'>Multi-Service</h1>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold py-3'>Provider</h1>
-        <p className='md:text-2xl text-xl text-[#1D5B79]'>Quircom is always on the lookout for like-minded visionaries and business es eager to make their mark in the world.</p>
-        <hr className='w-[50%] bg-black-600 border-0 rounded'/>
-        <div>
-          <img src={Mascot} alt="" className='w-[100%] h-[100%] relative z-[5]' />
+      <div className="max-w-[1240px] h-screen mx-auto flex flex-col mt-6 text-start justify-normal">
+        <div className='flex flex-row'>
+          <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold pl-6 pr-3 text-[#1D5B79]'>Multi -</h1>
+          <span className='md:text-7xl sm:text-6xl text-4xl font-bold  text-orange-600'>Service</span>
         </div>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold px-6 text-[#1D5B79]'>Provider</h1>
+        <p className='max-w-[470px] px-6 mt-10 text-[#1D5B79]'>
+          Quircom is always on the lookout for like-minded visionaries and business es eager to make their mark in the world.
+        </p>
+        <div className='flex-1 w-[150px] h-[50px] p-[2px] m-[20px] text-'>
+          <p className='font-bold text-[18px]'>Learn More!</p>
+        </div>
+        <hr className='w-[50%] bg-black-600 border-0 rounded'/>
       </div>
   )
   }
