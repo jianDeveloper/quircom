94c3b4fe85cc59330071a3fc5ba7d88767582aea
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-27 22:23:49 +0800

    finishing mini-menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,17 +16,17 @@ const navBar = () => {
     <div className ='header'>
       <Link to='/'><img className='h-[60px]' src={logo1}/></Link>
       <ul className='hidden md:flex'>
-        <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"} className='text-[#1D5B79]'>About us</Link></li>
-        <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Careers</Link></li>
-        <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Services</Link></li>
-        <li className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Testimonials</Link></li>
+        <li onClick={() => setNav(false)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"} className='text-[#1D5B79]'>About us</Link></li>
+        <li onClick={() => setNav(false)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Careers</Link></li>
+        <li onClick={() => setNav(false)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Services</Link></li>
+        <li onClick={() => setNav(false)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'><Link to={"/"}>Testimonials</Link></li>
       </ul>
-      
+
       {/* Log-Reg */}
       <div className='hidden md:flex '>
       <button onClick={() => setLogin(true)} className=' pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#303030]'>Log In</button>
       <Modal open={openLogin} onClose={() => setLogin(false)}/>
-      <button className='pt-2 pb-2 pl-3 pr-3 text-center font-bold'>
+      <button onClick={() => setNav(false)} className='pt-2 pb-2 pl-3 pr-3 text-center font-bold'>
         <Link to={'/registration'} className='text-white bg-[#1D5B79] rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</Link>
       </button>
       </div>
@@ -38,15 +38,15 @@ const navBar = () => {
       <div className={nav ? 'fixed md:hidden left-0 top-0 w-[300px] h-full border-l-solid border-l-[15px] border-l-[#1D5B79] bg-white ease-in-out duration-500' : 'fixed left-[-100%]'}>
       <img className='h-[70px] m-[10px] pt-[9px]' src={logo1} />
         <ul>
-          <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'>About us</li>
-          <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Careers</a></li>
-          <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Services</a></li>
-          <li className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Testimonials</a></li>
+          <li onClick={() => setNav(false)} className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>About us</a></li>
+          <li onClick={() => setNav(false)} className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Careers</a></li>
+          <li onClick={() => setNav(false)} className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Services</a></li>
+          <li onClick={() => setNav(false)} className='p-4 border-b-2 ml-6 mr-6 border-orange-600'><a href='#'>Testimonials</a></li>
         </ul>
         <div className='flex-inline justify-center items-center mt-[50px]'> {/* Log-Reg */}
           <button onClick={() => setLogin(true)} className='flex mx-auto pt-2 pb-2 pl-3 pr-3 text-center font-bold text-[#1D5B79]'>Log In</button>
           <Modal open={openLogin} onClose={() => setLogin(false)}/>
-          <button className='flex mx-auto pt-2 pb-2 pl-3 pr-3 text-center font-bold'>
+          <button onClick={() => setNav(false)} className='flex mx-auto pt-2 pb-2 pl-3 pr-3 text-center font-bold'>
             <Link to={'/registration'} className='text-white bg-orange-600 rounded-[15px] cursor-pointer pl-[10px] pr-[10px] pt-[3px] pb-[5px]'>Sign Up</Link>
           </button> 
         </div>
