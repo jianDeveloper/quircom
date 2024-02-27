0fbbe3878633df725e799fb2b318b576a11b4cf7
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-28 00:24:07 +0800

    bg reg added and fixed

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+import BGreg from '../assets/bgreg.png';
+
 const Reg = () => {
   return (
-    <div className='bg-white'>
+    <section className="">
+      <div className="h-screen" style={{background: `url(${BGreg})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
       <h1>Hello World</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim blanditiis atque iste commodi sint exercitationem laudantium temporibus quos! Nobis, veniam voluptate. A nulla sequi dignissimos deleniti, suscipit obcaecati veniam necessitatibus.</p>
     </div>
+    </section>
+    
   )
 }
 
