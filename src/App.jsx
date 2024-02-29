00a4055b1089b4864644f1a39f79e32bb40b971e
Ji-an <johnnicolebergantinos@gmail.com>
2024-02-29 21:58:13 +0800

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar.jsx';
@@ -6,12 +7,15 @@ import Hero from './components/Hero.jsx';
 import Reg from './components/Reg.jsx';
 
 function App() {
+  const [isLogin, setUserNavbar] = useState(false);
   return (
     <div>
-      <Navbar />
+      {isLogin ? <UserNavbar /> : <Navbar />}
       <Routes>
         <Route exact path='/' element={<Hero />} />
         <Route exact path='/registration' element={<Reg />} />
+        {/* onLogin={() => setUserNavbar(true)} */}
+
       </Routes>
     </div>
   )
