a3612f6b852c4f2e9c28426d0c8c2e7b5fd76de0
Ji-an <johnnicolebergantinos@gmail.com>
2024-03-04 00:28:26 +0800

    font

--- src/components/Client/CNav.jsx
+++ src/components/Client/CNav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const CNav = () => {
   return (
     <div>
-        <h1>hello world</h1>
+        <div class="sticky w-[60px] mt-[10%]">
+            <nav class="flex flex-col justify-center items-center px-2 py-8 bg-[white] rounded-r-2xl shadow-md border-solid  border-2">
+                <FontAwesomeIcon icon="fa-solid fa-circle-user" className='mb-8 px-2 py-4' style={{color: "#163646"}} />
+                <FontAwesomeIcon icon="fa-solid fa-chart-simple" className='px-2 py-4' style={{color: "#163646"}} />
+                <FontAwesomeIcon icon="fa-solid fa-briefcase" className='px-2 py-4' style={{color: "#163646"}} />
+                <FontAwesomeIcon icon="fa-solid fa-bars-progress" className=' px-2 py-4' style={{color: "#163646"}} />
+                <FontAwesomeIcon icon="fa-solid fa-bell" className=' mt-[100px] px-2 py-4' style={{color: "#163646"}} />
+                <FontAwesomeIcon icon="fa-solid fa-gear" className=' px-2 py-4' style={{color: "#163646"}} />
+            </nav>
+        </div>
     </div>
   )
 }
