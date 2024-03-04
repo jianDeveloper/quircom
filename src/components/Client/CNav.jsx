ffbc5229a0695e547f9e1ed25e4277fe30d98b2f
Ji-an <johnnicolebergantinos@gmail.com>
2024-03-04 19:49:01 +0800

    cdashboard

--- src/components/Client/CNav.jsx
+++ src/components/Client/CNav.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import navLogo from './assets/QRM1.png';
 
 
 const CNav = () => {
   return (
-    <div>
-        <div class="sticky w-[60px] mt-[10%]">
-            <nav class="flex flex-col justify-center items-center px-2 py-8 bg-[white] rounded-r-2xl shadow-md border-solid  border-2">
-                <FontAwesomeIcon icon="fa-solid fa-circle-user" className='mb-8 px-2 py-4' style={{color: "#163646"}} />
-                <FontAwesomeIcon icon="fa-solid fa-chart-simple" className='px-2 py-4' style={{color: "#163646"}} />
-                <FontAwesomeIcon icon="fa-solid fa-briefcase" className='px-2 py-4' style={{color: "#163646"}} />
-                <FontAwesomeIcon icon="fa-solid fa-bars-progress" className=' px-2 py-4' style={{color: "#163646"}} />
-                <FontAwesomeIcon icon="fa-solid fa-bell" className=' mt-[100px] px-2 py-4' style={{color: "#163646"}} />
-                <FontAwesomeIcon icon="fa-solid fa-gear" className=' px-2 py-4' style={{color: "#163646"}} />
-            </nav>
-        </div>
+    <div className='flex'>
+      <div class={`w-72 h-screen bg-orange-600`}>
+        <image className='h-[30px] bg-white' src={navLogo}/>
+      </div>
+      <div className='p-7 text-2xl font-semibold'>
+        <h1>Client</h1>
+      </div>
     </div>
   )
 }
