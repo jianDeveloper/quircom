799a6a355bb258c6fb796286e1da87bba391971c
vinsu <vencepuno.work@gmail.com>
2024-03-11 20:28:52 +0800

    stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -4,7 +4,7 @@ import styles from "../styles";
 const Stats =()=>{
     return(
         <div className="lg:col-span-3 justify-center h-[75%] text-xl xl:text-sm flex flex-wrap">
-            <div className='max-w-[1240px] flex1 flex items-center my-6'> 
+            <div className='max-w-[1240px] flex-1 flex items-center my-6'> 
                 <h4 className='font-semibold md:text-6xl sm:text-3xl text-4xl text-[#1D5B79] px-4'>30<span className='text-[2rem] align-middle'>+</span></h4>
                 <p className='font-semibold sm:text-[25px] text-[30px] md:leading-[26px] leading-[21px] text-orange-600'>Users</p>
             </div>
