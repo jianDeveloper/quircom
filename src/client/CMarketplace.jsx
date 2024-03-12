1aa5068c39aa810c9dc59a5b543341767d0f071b
Ji-an <136402729+xvinsue@users.noreply.github.com>
2024-03-12 09:23:52 +0800

    update

--- src/client/CMarketplace.jsx
+++ src/client/CMarketplace.jsx
@@ -12,7 +12,7 @@ function CMarketplace() {
             <h1 className='font-extrabold text-[30px] text-[#1D5B79]' >BROWSE</h1>
             <div className="w-full h-[300px]" style={{background: `url(${mpTop})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
                 <h1 className=''>Freelancing Services</h1>
-                <a href="#">Browse</a>
+                <a href="#">Browsers</a>
             </div>
         </main>
     </div>
