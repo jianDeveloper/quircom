24e94144b6c131616a6ef49178b090e5f9edf3e9
James De Sena <jamesdesena27@gmail.com>
2024-03-14 12:00:38 +0800

    done profile

--- src/client/CProfile.jsx
+++ src/client/CProfile.jsx
@@ -62,9 +62,6 @@ function CProfile() {
             <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
             <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
             <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
-            <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
-            <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
-            <p className=' mx-9 my-2 text-black text-[15px] text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed porta turpis. In cursus libero et sem sagittis aliquet. </p>
           </div>
         </div>
         </main>
