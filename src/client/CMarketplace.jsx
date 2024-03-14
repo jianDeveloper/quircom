60d52e88db9aad0eb9896611b86f3984602070c4
vinsu <vencepuno.work@gmail.com>
2024-03-14 10:52:29 +0800

    done marketplace

--- src/client/CMarketplace.jsx
+++ src/client/CMarketplace.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 import Header from './Header'
 import CNav from './CNav'
 import mpTop from '../assets/mpTop.jpg'
+import CCards from './CCards'
+import mpCard from '../assets/laptop.jpg'
 
-function CMarketplace() {
-
+const CMarketplace = () => {
+  const data = [
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    { image: mpCard, subtitle:'Logo Design', title:'The Firepit', author:'by Naruto', button:'Connect'  },
+    // Add more items as needed
+  ];
   return (
     <div className=''>
       <Header />
@@ -15,13 +27,15 @@ function CMarketplace() {
         <h1 className='font-extrabold text-[30px] text-[#1D5B79]' >MARKETPLACE</h1>
         <div className="w-full h-[300px] relative rounded-lg" style={{background: `url(${mpTop})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
           <div className="absolute bottom-10 left-10 text-white p-2">
-            <h1 className='text-xl font-bold mb-5'>Freelancing Services</h1>
+            <h1 className='text-[40px] font-bold mb-5'>Freelancing Services</h1>
             <a href="#" className='bg-orange-600 p-2 rounded-md'>Hire now!</a>
           </div>
         </div>
         <h1 className='mt-10 font-extrabold text-[30px] text-[#1D5B79]'>Top Services</h1>
-        <div className='w-full overflow-auto hover:overflow-scroll'>
-        
+        <div className='w-full h-[43vh] overflow-scroll  grid grid-cols-4 gap-4'>
+        {data.map((item, index) => (
+        <CCards key={index} image={item.image} subtitle={item.subtitle} title={item.title} author={item.author}  button={item.button}/>
+      ))}
         </div>
       </main>
       </div>
