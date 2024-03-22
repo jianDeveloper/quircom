0941d1a867394bc6f3d292e781bd44aa458ecfdf
James De Sena <jamesdesena27@gmail.com>
2024-03-22 21:56:42 +0800

    Latest Update Backend

--- frontend/src/components/Reg.jsx
+++ frontend/src/components/Reg.jsx
@@ -41,6 +41,18 @@ const Reg = () => {
 
     // Validation checks
     const errors = {};
+    if (formData.firstName.length == 0) {
+      errors.firstName = 'Please input your first name'
+    }
+    if (formData.surName.length == 0) {
+      errors.surName = 'Please input your last name'
+    }
+    if (formData.userName.length == 0) {
+      errors.userName = 'Please input your username'
+    }
+    if (formData.passWord.length == 0) {
+      errors.passWord = 'Please input your password'
+    }
     if (formData.contactNum.length < 11) {
       errors.contactNum = 'Contact number must be at least 11 characters';
     }
@@ -76,7 +88,7 @@ const Reg = () => {
   return (
     <section className="">
       <div className="flex xl:none xl:justify-start h-screen mb-[200px] md:mb-0 justify-center" style={{background: `url(${BGreg})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-        <div className='flex flex-col mt-[60px] xl:ml-[150px] md:h-[600px] bg-[beige] bg-opacity-80 md:bg-[beige] w-screen md:w-[650px] px-[20px] py-[30px] rounded-[10px] md:shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)]'>
+        <div className='flex flex-col mt-[60px] xl:ml-[150px] md:h-[700px] bg-[beige] bg-opacity-80 md:bg-[beige] w-screen md:w-[650px] px-[20px] py-[30px] rounded-[10px] md:shadow-[2px_2px_5px_5px_rgba(0,0,0,0.15)]'>
         <h2 className='text-center mx-[20px] mt-[10px] text-[30px] text-[#1D5B79] font-extrabold drop-shadow-xl'>Create Account</h2>
           <div className="container mx-auto mt-8">
             <form className="w-full max-w-screen-ss mx-auto" onSubmit={handleSubmit}>
@@ -91,9 +103,10 @@ const Reg = () => {
                     name="firstName"
                     value={formData.firstName}
                     onChange={handleChange}
-                    className="w-full text-[12px] p-3 border rounded"
+                    className={`w-full text-[12px] p-3 border rounded ${invalidFields.contactNum ? 'border-red-500' : ''}`}
                     placeholder="Enter your first name"
                   />
+                  {invalidFields.firstName && <p className="text-red-500">{invalidFields.firstName}</p>}
                 </div>
                 <div className="w-full md:w-1/2 px-3 mb-4">
                   <label htmlFor="lastName" className="block text-[#1D5B79] text-sm font-bold mb-2">
@@ -105,9 +118,10 @@ const Reg = () => {
                     name="surName"
                     value={formData.surName}
                     onChange={handleChange}
-                    className="w-full text-[12px] p-3 border rounded"
+                    className={`w-full text-[12px] p-3 border rounded ${invalidFields.surName ? 'border-red-500' : ''}`}
                     placeholder="Enter your last name"
                   />
+                  {invalidFields.surName && <p className="text-red-500">{invalidFields.surName}</p>}
                 </div>
               </div>
 
@@ -122,9 +136,10 @@ const Reg = () => {
                     name="userName"
                     value={formData.userName}
                     onChange={handleChange}
-                    className="w-full text-[12px] p-3 border rounded"
+                    className={`w-full text-[12px] p-3 border rounded ${invalidFields.userName ? 'border-red-500' : ''}`}
                     placeholder="Enter your username"
                   />
+                  {invalidFields.userName && <p className="text-red-500">{invalidFields.userName}</p>}
                 </div>
                 <div className="w-full md:w-1/2 px-3 mb-4">
                   <label htmlFor="password" className="block text-[#1D5B79] text-sm font-bold mb-2">
@@ -136,9 +151,10 @@ const Reg = () => {
                     name="passWord"
                     value={formData.passWord}
                     onChange={handleChange}
-                    className="w-full p-2 border rounded"
+                    className={`w-full p-2 border rounded ${invalidFields.passWord ? 'border-red-500' : ''}`}
                     placeholder="Enter your password"
                   />
+                  {invalidFields.passWord && <p className="text-red-500">{invalidFields.passWord}</p>}
                 </div>
               </div>
 
@@ -156,7 +172,7 @@ const Reg = () => {
                     className={`w-full text-[14px] p-2.5 border rounded ${invalidFields.contactNum ? 'border-red-500' : ''}`}
                     placeholder="Enter your contact number"
                   />
-                  {invalidFields.contactNum && <p className="text-red-500 text-sm">Contact Number must be atleast 11 characters.</p>}
+                  {invalidFields.contactNum && <p className="text-red-500 text-sm">{invalidFields.contactNum}</p>}
                 </div>
                 <div className="w-full md:w-1/2 px-3 mb-4">
                   <label htmlFor="country" className="block text-[#1D5B79] text-sm font-bold mb-2">
@@ -167,12 +183,11 @@ const Reg = () => {
                     name="country"
                     value={formData.country}
                     onChange={handleChange}
-                    className="w-full p-2 border rounded"
-                  >
+                    className={`w-full p-2 border rounded ${invalidFields.contactNum ? 'border-red-500' : ''}`}>
                     <option value="">Select Country</option>
                     <option value="Philippines">Philippines</option>
-        
                   </select>
+                  {invalidFields.country && <p className="text-red-500 text-sm">{invalidFields.country}</p>}
                 </div>
               </div>
 
@@ -187,9 +202,10 @@ const Reg = () => {
                     name="eMail"
                     value={formData.eMail}
                     onChange={handleChange}
-                    className="w-full text-[12px] p-3  border rounded"
+                    className={`w-full text-[12px] p-3  border rounded ${invalidFields.eMail ? 'border-red-500' : ''}`}
                     placeholder="Enter your email"
                   />
+                  {invalidFields.contactNum && <p className="text-red-500 text-sm">{invalidFields.eMail}</p>}
                 </div>
                 <div className="w-full md:w-1/2 px-3 mb-3">
                   <label htmlFor="accountType" className="block text-[#1D5B79] text-sm font-bold mb-2">
@@ -200,12 +216,12 @@ const Reg = () => {
                     name="accType"
                     value={formData.accType}
                     onChange={handleChange}
-                    className="w-full p-2 border rounded"
-                  >
+                    className={`w-full p-2 border rounded ${invalidFields.accType ? 'border-red-500' : ''}`}>
                     <option value="">Select Account Type</option>
                     <option value="freelancer">Freelancer</option>
                     <option value="client">Client</option>
                   </select>
+                  {invalidFields.accType && <p className="text-red-500 text-sm">{invalidFields.accType}</p>}
                 </div>
               </div>
               <div className="mb-6">
@@ -216,12 +232,13 @@ const Reg = () => {
                     name="aggRee"
                     checked={formData.aggRee}
                     onChange={handleChange}
-                    className="mr-2"
-                  />
+                    className={`mr-2 ${invalidFields.aggRee ? 'border-red-500' : ''}`}
+                  />  
                   <label htmlFor="agree" className='text-[14px]'>
                     I agree to the <Link className='text-[#1D5B79] font-medium'><u>terms and conditions</u></Link>
-                  </label>
+                  </label>    
                 </div>
+                {invalidFields.aggRee && <p className="text-red-500 text-center text-sm">{invalidFields.aggRee}</p>}
               </div>
               <div className="flex justify-center">
                 <button
