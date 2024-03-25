8229f98a14b75c40c91ea67474f91c406897dba1
vinsu <vencepuno.work@gmail.com>
2024-03-25 15:23:09 +0800

    csa

--- src/client/CCards.jsx
+++ src/client/CCards.jsx
@@ -3,25 +3,25 @@ import { Card, CardHeader, CardBody, CardFooter, Typography, Button } from '@mat
 
 const CCards = ({ image, subtitle, title, author, button }) => {
   return (
-    <Card className="mt-6 w-[320px]">
-      <CardHeader className="relative h-55">
-        <img className="rounded-md w-full" src={image} alt={title} />
-      </CardHeader>
-      <CardBody className='flex flex-row justify-between items-center '>
-        <div className='flex flex-col'>
-            <Typography className="mb-2 text-orange-600">
-                {subtitle}
-            </Typography>
-            <Typography className="mb-2 text-[#1D5B79] font-bold text-3xl">
-                {title}
-            </Typography>
-            <Typography className='text-[#1D5B79]'>
-                {author}
-            </Typography>
-        </div>
-        <Button className='bg-orange-600 h-10 w-50 text-center p-2 '>{button}</Button>
-      </CardBody>
-    </Card>
+      <Card className="mt-6 w-[320px]">
+        <CardHeader className="relative h-55">
+          <img className="rounded-md w-full" src={image} alt={title} />
+        </CardHeader>
+        <CardBody className='flex flex-row justify-between items-center '>
+          <div className='flex flex-col'>
+              <Typography className="mb-2 text-orange-600">
+                  {subtitle}
+              </Typography>
+              <Typography className="mb-2 text-[#1D5B79] font-bold text-3xl">
+                  {title}
+              </Typography>
+              <Typography className='text-[#1D5B79]'>
+                  {author}
+              </Typography>
+          </div>
+          <Button className='bg-orange-600 h-10 w-50 text-center p-2 '>{button}</Button>
+        </CardBody>
+      </Card>
   );
 };
 
