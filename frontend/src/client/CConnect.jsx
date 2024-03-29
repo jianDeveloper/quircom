cbe00fac8b601b309f0cf4555121162632d52da1
JannKlyde <102938104+JannKlyde@users.noreply.github.com>
2024-03-29 14:49:23 +0800

    Update CConnect.jsx

--- frontend/src/client/CConnect.jsx
+++ frontend/src/client/CConnect.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import mpCard1 from '../assets/pic1.png'
 
 function CConnect () {
   return (
@@ -8,24 +9,24 @@ function CConnect () {
       <ol role="list" class="flex items-center">
         <li class="text-left">
           <div class="-m-1">
-            <a href="#" class="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"> Home </a>
+            <a href="#" class="rounded-md p-1 text-sm font-medium text-black-600 focus:text-gray-900 focus:shadow hover:bg-[#EA580C]"> Home </a>
           </div>
         </li>
 
         <li class="text-left">
           <div class="flex items-center">
-            <span class="mx-2 text-gray-400">/</span>
+            <span class="mx-2 text-black-400">/</span>
             <div class="-m-1">
-              <a href="#" class="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"> Products </a>
+              <a href="#" class="rounded-md p-1 text-sm font-medium text-black-600 focus:text-gray-900 focus:shadow hover:bg-[#EA580C]"> Service </a>
             </div>
           </div>
         </li>
 
         <li class="text-left">
           <div class="flex items-center">
-            <span class="mx-2 text-gray-400">/</span>
+            <span class="mx-2 text-black-400">/</span>
             <div class="-m-1">
-              <a href="#" class="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800" aria-current="page"> Coffee </a>
+              <a href="#" class="rounded-md p-1 text-sm font-medium text-black-600 focus:text-gray-900 focus:shadow hover:bg-[#EA580C]" aria-current="page"> Settings </a>
             </div>
           </div>
         </li>
@@ -37,20 +38,20 @@ function CConnect () {
         <div class="lg:flex lg:items-start">
           <div class="lg:order-2 lg:ml-5">
             <div class="max-w-xl overflow-hidden rounded-lg">
-              <img class="h-full w-full max-w-full object-cover" src="/images/JHxMnVrtPMdcNU1s_7g7f.png" alt="" />
+              <img class="h-full w-full max-w-full object-cover" src={mpCard1} alt="" />
             </div>
           </div>
 
           <div class="mt-2 w-full lg:order-1 lg:w-32 lg:flex-shrink-0">
             <div class="flex flex-row items-start lg:flex-col">
               <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-gray-900 text-center">
-                <img class="h-full w-full object-cover" src="/images/JHxMnVrtPMdcNU1s_7g7f.png" alt="" />
+                <img class="h-full w-full object-cover" src={mpCard1} alt="" />
               </button>
               <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
-                <img class="h-full w-full object-cover" src="/images/JHxMnVrtPMdcNU1s_7g7f.png" alt="" />
+                <img class="h-full w-full object-cover" src={mpCard1} alt="" />
               </button>
               <button type="button" class="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-transparent text-center">
-                <img class="h-full w-full object-cover" src="/images/JHxMnVrtPMdcNU1s_7g7f.png" alt="" />
+                <img class="h-full w-full object-cover" src={mpCard1} alt="" />
               </button>
             </div>
           </div>
@@ -58,7 +59,8 @@ function CConnect () {
       </div>
 
       <div class="lg:col-span-2 lg:row-span-2 lg:row-end-2">
-        <h1 class="sm: text-2xl font-bold text-gray-900 sm:text-3xl">Afro-Brazillian Coffee</h1>
+        <h1 class="sm: text-2xl font-bold text-gray-900 sm:text-3xl">UI/UX Design</h1>
+        <p class="sm: text-xl font-itallic text-gray-900 sm:text-xl">Naruto Shippuden</p>
 
         <div class="mt-5 flex items-center">
           <div class="flex items-center">
@@ -81,48 +83,13 @@ function CConnect () {
           <p class="ml-2 text-sm font-medium text-gray-500">1,209 Reviews</p>
         </div>
 
-        <h2 class="mt-8 text-base text-gray-900">Coffee Type</h2>
-        <div class="mt-3 flex select-none flex-wrap items-center gap-1">
-          <label class="">
-            <input type="radio" name="type" value="Powder" class="peer sr-only" checked />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Powder</p>
-          </label>
-          <label class="">
-            <input type="radio" name="type" value="Whole Bean" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Whole Bean</p>
-          </label>
-          <label class="">
-            <input type="radio" name="type" value="Groud" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">Groud</p>
-          </label>
-        </div>
-
-        <h2 class="mt-8 text-base text-gray-900">Choose subscription</h2>
-        <div class="mt-3 flex select-none flex-wrap items-center gap-1">
-          <label class="">
-            <input type="radio" name="subscription" value="4 Months" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">4 Months</p>
-            <span class="mt-1 block text-center text-xs">$80/mo</span>
-          </label>
-          <label class="">
-            <input type="radio" name="subscription" value="8 Months" class="peer sr-only" checked />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">8 Months</p>
-            <span class="mt-1 block text-center text-xs">$60/mo</span>
-          </label>
-          <label class="">
-            <input type="radio" name="subscription" value="12 Months" class="peer sr-only" />
-            <p class="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">12 Months</p>
-            <span class="mt-1 block text-center text-xs">$40/mo</span>
-          </label>
-        </div>
-
         <div class="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
           <div class="flex items-end">
-            <h1 class="text-3xl font-bold">$60.50</h1>
+            <h1 class="text-3xl font-bold">₱999.00</h1>
             <span class="text-base">/month</span>
           </div>
 
-          <button type="button" class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800">
+          <button type="button" class="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-[#FE6D30] bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-[#EA580C]">
             <svg xmlns="http://www.w3.org/2000/svg" class="shrink-0 mr-3 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
@@ -135,7 +102,7 @@ function CConnect () {
             <svg class="mr-2 block h-5 w-5 align-middle text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" class=""></path>
             </svg>
-            Free shipping worldwide
+            Quircom Philippines Services 
           </li>
 
           <li class="flex items-center text-left text-sm font-medium text-gray-600">
