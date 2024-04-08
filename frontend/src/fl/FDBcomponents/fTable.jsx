7aefc88e92d990b2673431873e687c21c9b521ec
[Ji-an] <johnnicolebergantinos@gmail.com>
2024-04-08 23:03:17 +0800

    done table

--- frontend/src/fl/FDBcomponents/fTable.jsx
+++ frontend/src/fl/FDBcomponents/fTable.jsx
@@ -1,53 +1,125 @@
-import React from 'react'
-import { DataGrid } from '@mui/x-data-grid';
+import * as React from 'react';
+import {Paper, Table, TableBody, TableContainer, TableHead, TablePagination, TableRow, TableCell} from '@mui/material/';
+
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value) => value.toFixed(2),
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-const fTable = () => {
-  return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
-  )
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
 }
 
-export default fTable
+const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+  createData('Canada', 'CA', 37602103, 9984670),
+  createData('Australia', 'AU', 25475400, 7692024),
+  createData('Germany', 'DE', 83019200, 357578),
+  createData('Ireland', 'IE', 4857000, 70273),
+  createData('Mexico', 'MX', 126577691, 1972550),
+  createData('Japan', 'JP', 126317000, 377973),
+  createData('France', 'FR', 67022000, 640679),
+  createData('United Kingdom', 'GB', 67545757, 242495),
+  createData('Russia', 'RU', 146793744, 17098246),
+  createData('Nigeria', 'NG', 200962417, 923768),
+  createData('Brazil', 'BR', 210147125, 8515767),
+];
+
+export default function fTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={2}>
+                Country
+              </TableCell>
+              <TableCell align="center" colSpan={3}>
+                Details
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ top: 57, minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
--- frontend/src/fl/FDashboard.jsx
+++ frontend/src/fl/FDashboard.jsx
@@ -1,151 +1,270 @@
-import { useEffect, useState, useContext } from 'react';
-import React from 'react';
-import axios from 'axios'; // Import axios for making HTTP requests
-import UserContext from '../context/UserContext';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState, useContext } from "react";
+import React from "react";
+import axios from "axios"; // Import axios for making HTTP requests
+import UserContext from "../context/UserContext";
+import { useParams } from "react-router-dom";
 
-import fTable from '../fl/FDBcomponents/fTable.jsx';
-import BG1 from '../assets/bg1.png';
-import { MdDesignServices,MdPendingActions } from "react-icons/md";
+import BG1 from "../assets/bg1.png";
+import { MdDesignServices, MdPendingActions } from "react-icons/md";
 import { FaFileCircleCheck } from "react-icons/fa6";
-import { DataGrid } from '@mui/x-data-grid';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  TableCell,
+} from "@mui/material/";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { id: "ticketID", label: "Ticket ID", minWidth: 100, align: "center" },
+  { id: "cName", label: "Client Name", minWidth: 170 },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
+    id: "availedService",
+    label: "Availed Service",
+    minWidth: 100,
+    align: "center",
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    id: "task",
+    label: "Task Details",
+    minWidth: 200,
+    align: "left",
+  },
+  {
+    id: "deadline",
+    label: "Deadline",
+    minWidth: 100,
+    align: "center",
+    format: (value) => new Date(value).toLocaleString("en-GB"),
+  },
+  {
+    id: "status",
+    label: "Status",
+    minWidth: 100,
+    align: "center",
+  },
+  {
+    id: "actions",
+    label: "Actions",
+    minWidth: 100,
+    align: "center",
+    format: (value) => (
+      <div>
+        <button type="button" className="mr-2 px-2 py-1 bg-blue-500 rounded text-white">
+          <MdDesignServices className="inline" />
+        </button>
+        <button type="button" className="px-2 py-1 bg-orange-500 rounded text-white">
+          <FaFileCircleCheck className="inline" />
+        </button>
+      </div>
+    ),
   },
 ];
 
+function createData(ticketID, cName, availedService, task, deadline, status) {
+  return { ticketID, cName, availedService, task, deadline, status };
+}
+
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  createData("123456789", "John Nicole Bergantinos", "Graphics Design", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione quam dicta vel natus odit. Tenetur officia, nam voluptate sint a eveniet minima in, dolore quaerat amet magni consectetur ea. Quisquam!", "2024-02-11", "Open"),
+  createData("12345", "CN", "IN", 1403500365, "2024-05-09", "Open"),
+  createData("12345", "IT", "IN", 60483973, "2024-02-15", "Open"),
+  createData("12345", "US", "IN", 327167434, "2024-08-09", "Open"),
+  createData("12345", "CA", "IN", 37602103, "2024-01-12", "Open"),
+  createData("12345", "AU", "IN", 25475400, "2024-04-25", "Open"),
+  createData("12345", "DE", "IN", 83019200, "2024-07-28", "Open"),
+  createData("12345", "IE", "IN", 4857000, "2024-01-10", "Open"),
+  createData("12345", "MX", "IN", 126577691, "2024-06-09", "Open"),
+  createData("12345", "JP", "IN", 126317000, "2024-01-09", "Open"),
+  createData("12345", "FR", "IN", 67022000, "2024-02-22", "Open"),
+  createData("12345", "GB", "IN", 67545757, "2024-05-10", "Open"),
+  createData("12345", "RU", "IN", 146793744, "2024-09-11", "Open"),
+  createData("12345", "NG", "IN", 200962417, "2024-04-09", "Open"),
+  createData("12345", "BR", "IN", 210147125, "2024-03-19", "Open"),
 ];
 
 function FDashboard() {
   const { userId } = useParams();
-  const [ userData, setUserData] = useState(null); // State to store user data
-  
-  const [activeTab, setActiveTab] = useState('track');
+  const [userData, setUserData] = useState(null); // State to store user data
+
+  const [activeTab, setActiveTab] = useState("track");
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   const handleTab = (track) => {
     setActiveTab(track);
   };
 
   const { userIdLink } = useContext(UserContext);
-  console.log('User ID in Dashboard:', userIdLink);
-
+  console.log("User ID in Dashboard:", userIdLink);
 
   useEffect(() => {
     // Fetch user data using the user ID
-    axios.get(`http://localhost:8800/api/users/${userId}`)
-      .then(response => {
-        console.log('User data:', response.data);
+    axios
+      .get(`http://localhost:8800/api/users/${userId}`)
+      .then((response) => {
+        console.log("User data:", response.data);
         setUserData(response.data); // Set the user data in state
       })
-      .catch(error => {
-        console.error('Error fetching user data:', error);
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
       });
   }, [userId]); // Fetch user data whenever userId changes
 
-  
-
   return (
-    <div className='flex flex-col h-[100vh]' style={{ background: `url(${BG1})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-     {/* insert NAvbar  */}
-      <div className='flex align-center justify-center mx-[100px]'>
-        <div className='flex flex-col container my-10'> {/*formatting navbar & body -j*/}
-
+    <div
+      className="flex flex-col h-[100vh]"
+      style={{
+        background: `url(${BG1})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      {/* insert NAvbar  */}
+      <div className="flex align-center justify-center mx-[100px]">
+        <div className="flex flex-col container my-10">
+          {" "}
+          {/*formatting navbar & body -j*/}
           {/* Box Analytics */}
           <div className="grid grid-cols-3 gap-[20px] my-[15px]">
-            <div className='flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md'>
-              <div className='flex items-center justify-between'>
-                <h1 className='text-[20px] text-[#13334C] font-bold'>My Services</h1>
-                <MdDesignServices size={30} color='#1d5b79'/>
+            <div className="flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md">
+              <div className="flex items-center justify-between">
+                <h1 className="text-[20px] text-[#13334C] font-bold">
+                  My Services
+                </h1>
+                <MdDesignServices size={30} color="#1d5b79" />
               </div>
               <h1 className="font-medium text-[#1D5B79]">100</h1>
             </div>
-            <div className='flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md'>
-              <div className='flex items-center justify-between'>
-                <h1 className='text-[20px] text-[#13334C] font-bold'>Pending Projects</h1>
-                <MdPendingActions size={30} color='#1d5b79'/>
+            <div className="flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md">
+              <div className="flex items-center justify-between">
+                <h1 className="text-[20px] text-[#13334C] font-bold">
+                  Pending Projects
+                </h1>
+                <MdPendingActions size={30} color="#1d5b79" />
               </div>
               <h1 className="font-medium text-[#1D5B79]">33</h1>
             </div>
-            <div className='flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md'>
-              <div className='flex items-center justify-between'>
-                <h1 className='text-[20px] text-[#13334C] font-bold'>Finished Projects</h1>
-                <FaFileCircleCheck size={27} color='#1d5b79'/>
-              </div> 
+            <div className="flex flex-col justify-around px-4 py-4 border-[#1D5B79] border-[3px] border-solid bg-white hover:shadow-lg rounded-md">
+              <div className="flex items-center justify-between">
+                <h1 className="text-[20px] text-[#13334C] font-bold">
+                  Finished Projects
+                </h1>
+                <FaFileCircleCheck size={27} color="#1d5b79" />
+              </div>
               <h1 className="font-medium text-[#1D5B79]">12</h1>
             </div>
           </div>
           {/* Box Analytics */}
-            
           {/* Menu Tabs */}
-          <div className='flex flex-col justify-center items-center'>
+          <div className="flex flex-col justify-center items-center">
             {/* outside container */}
-            <div className='flex justify-between items-center w-[100%] h-[100px] my-5'>
-              <button className={activeTab === 'track' ? 'active-tab border-[#1D5B79] text-white bg-[#13334C] py-8 w-[49%] border-[3px] rounded-md' :
-              'border-[#1D5B79] py-4 w-[48%] border-[3px] rounded-md hover:w-[49%] hover:py-8 hover:bg-[#13334C] hover:text-white'}
-              onClick={() => handleTab('track')} >
-                <h1 className=' text-lg font-extrabold'>Ticket Tracker</h1>
+            <div className="flex justify-between items-center w-[100%] h-[100px] my-5">
+              <button
+                className={
+                  activeTab === "track"
+                    ? "active-tab border-[#1D5B79] text-white bg-[#13334C] py-8 w-[49%] border-[3px] rounded-md"
+                    : "border-[#1D5B79] py-4 w-[48%] border-[3px] rounded-md hover:w-[49%] hover:py-8 hover:bg-[#13334C] hover:text-white"
+                }
+                onClick={() => handleTab("track")}
+              >
+                <h1 className=" text-lg font-extrabold">Ticket Tracker</h1>
               </button>
-              <button className={activeTab === 'manage' ? 'active-tab border-[#1D5B79] text-white bg-[#13334C] py-8 w-[49%] border-[3px] rounded-md' : 
-              'border-[#1D5B79] py-4 w-[48%] border-[3px] rounded-md hover:w-[49%] hover:py-8 hover:bg-[#13334C] hover:text-white'} 
-              onClick={() => handleTab('manage')}>
-                <h1 className=' text-lg font-extrabold'>Manage Services</h1>
+              <button
+                className={
+                  activeTab === "manage"
+                    ? "active-tab border-[#1D5B79] text-white bg-[#13334C] py-8 w-[49%] border-[3px] rounded-md"
+                    : "border-[#1D5B79] py-4 w-[48%] border-[3px] rounded-md hover:w-[49%] hover:py-8 hover:bg-[#13334C] hover:text-white"
+                }
+                onClick={() => handleTab("manage")}
+              >
+                <h1 className=" text-lg font-extrabold">Manage Services</h1>
               </button>
             </div>
             {/* inside container */}
-            <div>
-              {activeTab === 'track' && (
-              <div style={{ height: 400, width: '100%' }}>
-                <DataGrid
-                  rows={rows}
-                  columns={columns}
-                  initialState={{
-                  pagination: {
-                  paginationModel: { page: 0, pageSize: 99 },
-                   },
-                 }}
-                 pageSizeOptions={[5, 10]}
-               />
-              </div>)}
-              {activeTab === 'manage' && (
-              <div className=''>
-                Rawr
-              </div>)}
+            <div className="flex flex-col justify-center items-center w-[90%]">
+              {activeTab === "track" && (
+                <div className="flex flex-col justify-center items-center w-[100%]">
+                  <Paper sx={{ width: "100%" }}>
+                  <TableContainer sx={{ height: 500, width: "100%", backgroundColor: "white" }}>
+  <Table stickyHeader aria-label="sticky table">
+    <TableHead>
+      <TableRow>
+        {columns.map((column) => (
+          <TableCell
+            key={column.id}
+            align={column.align}
+            style={{ minWidth: column.minWidth }}
+          >
+            {column.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {rows
+        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+        .map((row, rowIndex) => {
+          return (
+            <TableRow
+              hover
+              role="checkbox"
+              tabIndex={-1}
+              key={rowIndex}
+            >
+              {columns.map((column) => {
+                const value = row[column.id];
+                return (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                  >
+                    {column.id === "actions" ? column.format(value) : (
+                      column.format && typeof value === "number"
+                        ? column.format(value)
+                        : value
+                    )}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          );
+        })}
+    </TableBody>
+  </Table>
+</TableContainer>
+                    <TablePagination
+                      rowsPerPageOptions={[5, 10, 20]}
+                      component="div"
+                      count={rows.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                  </Paper>
+                </div>
+              )}
+              {activeTab === "manage" && <div className="">Rawr</div>}
             </div>
           </div>
           {/* Menu Tabs */}
-
         </div>
       </div>
       {/* Footer */}
-      
     </div>
-  )
+  );
 }
 
-export default FDashboard
+export default FDashboard;
