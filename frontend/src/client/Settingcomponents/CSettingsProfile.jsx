27b7740d66d16262a40a2fc53c159e1c3e7a23fb
vinsu <vencepuno.work@gmail.com>
2024-04-12 10:25:23 +0800

    add description input

--- frontend/src/client/Settingcomponents/CSettingsProfile.jsx
+++ frontend/src/client/Settingcomponents/CSettingsProfile.jsx
@@ -17,7 +17,7 @@ function CSettingsProfile(props) {
   ));
   
   const { userId } = useParams();
-  const [ userData, setUsers] = useState(null);
+  const [ userData, setUsers] = useState();
  
   console.log(userId)
   console.log('Display User:', userData)
@@ -25,7 +25,7 @@ function CSettingsProfile(props) {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get(`http://localhost:8800/api/client/${userId}`);
+        const response = await axios.get(`https://quircom.onrender.com/api/client/${userId}`);
         if (response.status === 200) {
           setUsers(response.data);
         }
@@ -80,36 +80,33 @@ function CSettingsProfile(props) {
             <div className="col-span-8 overflow-hidden rounded-xl sm:bg-[#F7F6DF] sm:px-8 sm:shadow">
               <div className="pt-4">
                 <h1 className="py-2 text-2xl font-semibold">Profile settings</h1>
-                <p className="font- text-slate-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p className="font- text-slate-600">Edit your information here.</p>
               </div>
               <hr className="mt-4 mb-8" />
               <p className="py-2 text-xl font-semibold">Account Handle</p>
               <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-                <p className="text-gray-600">Your account is <strong>John Doe</strong></p>
+              {userData && (
+                <p className="text-gray-600">Your account is <strong>{userData.firstName} {userData.surName}</strong></p>
+              )}
+
+                
               </div>
               <hr className="mt-4 mb-8" />
               {/* PORTFOLIO ZONE */}
-              <p className="py-2 text-xl font-semibold">Portfolio</p>
+              <p className="py-2 text-xl font-semibold">Profile Description</p>
               <div className="max-w-2xl">
-                <div className="flex flex-col items-center justify-center w-full">
-                  <div className='flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg bg-gray-50 dark:hover:bg-bray-800 hover:bg-gray-100'>
-                    <div {...getRootProps({className: 'dropzone flex flex-col items-center'})}>
-                      <input {...getInputProps()} />
-                      <svg className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                    </div>
-                  </div>
-                  <aside>
-                    <ul>{files}</ul>
-                  </aside>
-                </div>
+              
+                  <label class="block sm:col-span-2" for="message">
+                    
+                    <textarea class="h-32 w-full rounded-md border bg-white py-2 px-2 outline-none ring-yellow-500 focus:ring-2" type="text" placeholder="About you"></textarea>
+                  </label>
+                
               </div>
-              <button className="mt-4 rounded-lg bg-[#FE6D30] hover:bg-[#1D5B79] active:bg-blue-800 px-4 py-2 text-white">Save Portfolio</button>
+              <button className="mt-4 rounded-lg bg-[#FE6D30] hover:bg-[#1D5B79] active:bg-blue-800 px-4 py-2 text-white">Save</button>
               <hr className="mt-4 mb-8" />
 
               <div className="mb-10">
-                <p className="py-2 text-xl font-semibold">Delete Portfolio</p>
+                {/* <p className="py-2 text-xl font-semibold">Delete Portfolio</p> */}
                 <p className="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
                   <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"clipRule="evenodd" />
