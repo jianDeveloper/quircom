289b995f8c9a455644becba3e11ae3185e4c1947
vinsu <136402729+xvinsue@users.noreply.github.com>
2024-04-15 21:27:45 +0800

    modal

--- frontend/src/client/Subscomponents/AddBillModal.jsx
+++ frontend/src/client/Subscomponents/AddBillModal.jsx
@@ -2,60 +2,66 @@ import React from "react";
 
 const AddBillModal = ({ setbillModal }) => {
   return (
-    <div class="lg:m-10">
-  <form class="relative border border-gray-100 space-y-3 max-w-screen-md mx-auto rounded-md bg-white p-6 shadow-xl lg:p-10">
-  <h1 class="mb-6 text-xl font-semibold lg:text-2xl">Add Billing</h1>
-
-  <div class="grid gap-3 md:grid-cols-2">
-    <div> 
-      <label class=""> First Name </label>
-      <input type="text" placeholder="Your Name" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-    </div>
     <div>
-      <label class=""> Last Name </label>
-      <input type="text" placeholder="Last  Name" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-    </div>
-  </div>
-  <div>
-    <label class=""> Username </label>
-    <input type="text" placeholder="Username" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-  </div>
-  <div>
-    <label class=""> Email Address </label>
-    <input type="email" placeholder="Info@example.com" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-  </div>
-  <div>
-    <label class=""> Password </label>
-    <input type="password" placeholder="******" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-  </div>
-  <div class="grid gap-3 lg:grid-cols-2">
-    <div>
-      <label class=""> Gender </label>
-      <div class="relative w-56 mt-2 bg-gray-100 rounded-lg">
-        <input class="peer hidden" type="checkbox" name="select-1" id="select-1" />
-        <label for="select-1" class="flex w-full cursor-pointer rounded-lg select-none border p-2 px-3 text-sm text-gray-700 ring-blue-400 peer-checked:ring">Select Option </label>
-        <svg xmlns="http://www.w3.org/2000/svg" class="pointer-events-none absolute right-5 top-3 h-4 text-gray-600 transition peer-checked:rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-        </svg>
-        <ul class="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
-          <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Male</li>
-          <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Female</li>
-          <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Other</li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <label class=""> Phone: <span class="text-sm text-gray-400">(optional)</span> </label>
-      <input type="text" placeholder="+543 5445 0543" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
-    </div>
-  </div>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative w-2/4 my-6 mx-auto">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="flexitems-start justify-between p-5 bg-[#1d5b79] border-b border-solid border-blueGray-200 rounded-t">
+              <h3 className="text-3xl text-white text-center font-semibold">
+                Add Billing
+              </h3>
+            </div>
 
-  <div class="checkbox">
-    <input type="checkbox" id="chekcbox1" checked="" />
-    <label for="checkbox1">I agree to the <a href="#" target="_blank" class="text-blue-600"> Terms and Conditions </a> </label>
-  </div>
+            {/* Creating Form */}
+            <div class="">
+              <form class="relative border border-gray-100 space-y-3 max-w-screen-md mx-auto rounded-md bg-white p-6 lg:p-5">
 
-  {/* Add Close Button and Add Button */}
+              <div class="grid gap-3 md:grid-cols-2">
+                <div> 
+                  <label class=""> First Name </label>
+                  <input type="text" placeholder="Your Name" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
+                </div>
+                <div>
+                  <label class=""> Last Name </label>
+                  <input type="text" placeholder="Last  Name" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
+                </div>
+              </div>
+              <div>
+                <label class=""> Card Number </label>
+                <input type="text" placeholder="Card Number" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
+              </div>
+              
+              <div class="grid lg:grid-cols-2">
+                <div>
+                  <label class=""> Expiry Date </label>
+                  <div class="relative w-56 mt-2 bg-gray-100 rounded-lg">
+                    <input class="peer hidden" type="checkbox" name="select-1" id="select-1" />
+                    <label for="select-1" class="flex w-full cursor-pointer rounded-lg select-none border p-2 px-3 text-sm text-gray-700 ring-blue-400 peer-checked:ring">Select Option </label>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="pointer-events-none absolute right-5 top-3 h-4 text-gray-600 transition peer-checked:rotate-180" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+                    </svg>
+                    <ul class="max-h-0 select-none flex-col overflow-hidden rounded-b-lg shadow-md transition-all duration-300 peer-checked:max-h-56 peer-checked:py-3">
+                      <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Male</li>
+                      <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Female</li>
+                      <li class="cursor-pointer px-3 py-2 text-sm text-gray-500 hover:bg-blue-500 hover:text-white">Other</li>
+                    </ul>
+                  </div>
+                </div>
+                <div>
+                  <label class=""> CVV </label>
+                  <input type="text" placeholder="+543 5445 0543" class="mt-2 h-12 w-full rounded-md bg-gray-100 px-3" />
+                </div>
+              </div>
+
+              <div class="checkbox">
+                <input type="checkbox" id="chekcbox1" checked="" />
+                <label for="checkbox1">I agree to the <a href="#" target="_blank" class="text-blue-600"> Terms and Conditions </a> </label>
+              </div>
+            </form>
+
+            </div>
+
+            {/* Add Close Button and Add Button */}
             <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
               <button
                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -69,12 +75,14 @@ const AddBillModal = ({ setbillModal }) => {
                 type="button"
                 onClick={() =>  setbillModal (false)}
               >
-                Add Service
+                Pay Now
               </button>
             </div>
-</form>
-
-</div>
+          </div>
+        </div>
+      </div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </div>
   );
 };
 
