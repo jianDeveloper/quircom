6871221b54365bfe4c8b2ab420e80db4c94ce191
Jian <johnnicolebergantinos@gmail.com>
2024-04-16 14:34:10 +0800

    added error

--- frontend/src/fl/FDBcomponents/addServiceModal.jsx
+++ frontend/src/fl/FDBcomponents/addServiceModal.jsx
@@ -16,8 +16,9 @@ const AddServiceModal = ({ setaddModal }) => {
   // ];
 
   const { userId } = useParams();
-  const [ userData, setUsers ] = useState();
-  const [ thumbNail, setThumbnail ] = useState();
+  const [userData, setUsers] = useState();
+  const [thumbNail, setThumbnail] = useState();
+  const [invalidFields, setInvalidFields] = useState({});
 
   const [formData, setFormData] = useState({
     serviceName: "",
@@ -51,17 +52,35 @@ const AddServiceModal = ({ setaddModal }) => {
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent default form submission
 
+    
+    const errors = {};
+    if (formData.serviceName.length === 0) {
+      errors.serviceName = "Please input your Title";
+    }
+    if (!formData.serviceType) {
+      errors.serviceType = "Please select an Service type";
+    }
+    if (formData.serviceInfo.length === 20) {
+      errors.serviceInfo = "Please input atleast 20 characters";
+    }
+    if (!formData.price.length === 0) {
+      errors.price = "Please input your price";
+    }
+
     try {
       const formObj = new FormData();
       formObj.append("service", JSON.stringify(formData));
       formObj.append("file", thumbNail);
 
       const response = await axios.post(
-        `http://localhost:8800/api/service/create/`, formObj, {
+        `https://quircom.onrender.com/api/service/create/`,
+        formObj,
+        {
           headers: {
             "Content-Type": "multipart/form-data",
           },
-        });
+        }
+      );
 
       if (response && response.data) {
         console.log(response.data);
@@ -75,17 +94,19 @@ const AddServiceModal = ({ setaddModal }) => {
       console.log(error.message);
       toast.error("Failed to upload Service");
     }
+
+    setInvalidFields(errors);
   };
 
   const handleImage = (e) => {
     setThumbnail(e.target.files[0]);
-  }
+  };
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
@@ -93,7 +114,7 @@ const AddServiceModal = ({ setaddModal }) => {
 
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <div
         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
         style={{ background: "rgba(0,0,0,0.2)" }}
@@ -124,11 +145,11 @@ const AddServiceModal = ({ setaddModal }) => {
                     name="serviceName"
                     value={formData.serviceName}
                     onChange={handleChange}
-                    className="mt-1 relative rounded-md shadow-sm border border-gray-300 px-3 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
+                    className={`mt-1 relative rounded-md shadow-sm border border-gray-300 px-3 py-2 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm  ${invalidFields.serviceName ? "border-red-500" : ""}`}
                   />
                   <label
                     htmlFor="serviceType"
-                    className="block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300"
+                    className={`block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300`}
                   >
                     Service Type
                   </label>
@@ -138,7 +159,7 @@ const AddServiceModal = ({ setaddModal }) => {
                       name="serviceType"
                       value={formData.serviceType}
                       onChange={handleChange}
-                      className="block w-full px-3 py-2 pr-10 text-base leading-6 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                      className={`block w-full px-3 py-2 pr-10 text-base leading-6 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${invalidFields.serviceType ? "border-red-500" : ""}`}
                     >
                       <option value="">Select Account Type</option>
                       <option value="1">Web Development</option>
@@ -156,7 +177,7 @@ const AddServiceModal = ({ setaddModal }) => {
                   </div>
                   <label
                     htmlFor="serviceInfo"
-                    className="block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300"
+                    className={`block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300`}
                   >
                     Description
                   </label>
@@ -167,14 +188,14 @@ const AddServiceModal = ({ setaddModal }) => {
                       value={formData.serviceInfo}
                       onChange={handleChange}
                       rows={4}
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 px-3 py-2"
+                      className={`shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 px-3 py-2 ${invalidFields.serviceInfo ? "border-red-500" : ""}`}
                     />
                   </div>
                   <div className="flex flex-row justify-between gap-12">
                     <div className="w-[50%]">
                       <label
                         htmlFor="price"
-                        className="block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300"
+                        className={`block mt-4 text-md font-extrabold text-gray-700 pb-1 border-b border-gray-300`}
                       >
                         Price
                       </label>
@@ -183,12 +204,12 @@ const AddServiceModal = ({ setaddModal }) => {
                           PHP
                         </span>
                         <input
-                          type="text"
+                          type="number"
                           id="price"
                           name="price"
                           value={formData.price}
                           onChange={handleChange}
-                          className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-r-md sm:text-sm p-2 shadow-sm border border-gray-300"
+                          className={` focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-r-md sm:text-sm p-2 shadow-sm border border-gray-300 ${invalidFields.price ? "border-red-500" : ""}`}
                         />
                       </div>
                     </div>
@@ -209,7 +230,7 @@ const AddServiceModal = ({ setaddModal }) => {
                           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border border-gray-300 px-3 py-2"
                         />
                       </div>
-                    </div>                 
+                    </div>
                   </div>
                   <div className="flex flex-row justify-between gap-12">
                     <div className="w-[50%]">
@@ -224,7 +245,9 @@ const AddServiceModal = ({ setaddModal }) => {
                           type="text"
                           id="freelancerId"
                           name="freelancerId"
-                          value={`${userData?.firstName || ''} ${userData?.surName || ''}`}
+                          value={`${userData?.firstName || ""} ${
+                            userData?.surName || ""
+                          }`}
                           readOnly
                           className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-r-md sm:text-sm p-2 shadow-sm border border-gray-300"
                         />
@@ -242,14 +265,16 @@ const AddServiceModal = ({ setaddModal }) => {
                           type="text"
                           id="dateUploaded"
                           name="dateUploaded"
-                          value={new Date(formData.dateUploaded).toLocaleString()}
+                          value={new Date(
+                            formData.dateUploaded
+                          ).toLocaleString()}
                           readOnly
                           className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-r-md sm:text-sm p-2 shadow-sm border border-gray-300"
                         />
                       </div>
                     </div>
-                  </div>   
-                </div>               
+                  </div>
+                </div>
               </div>
 
               {/* Add Close Button and Add Button */}
--- frontend/src/fl/FSettings.jsx
+++ frontend/src/fl/FSettings.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react'
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import NavHeader from './FMainNav'
 import FFooter from './FFooter';
