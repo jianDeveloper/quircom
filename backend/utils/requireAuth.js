1d8fb62865956fff027ddbc7fc160b6682102f79
James De Sena <jamesdesena27@gmail.com>
2024-04-24 14:51:30 +0800

    sadfasdfaew

--- backend/utils/requireAuth.js
+++ backend/utils/requireAuth.js
@@ -1,35 +1,41 @@
 const jwt = require("jsonwebtoken");
-const FreelancerModel = require("../models/FreelancerModel.js")
-const ClientModel = require("../models/ClientModel.js") // Assuming this is the correct import for ClientModel
+const FreelancerModel = require("../models/FreelancerModel.js");
+const ClientModel = require("../models/ClientModel.js");
 
 const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
 
   // Checks if there is a token available for authentication
   if (!authorization) {
-      return res.status(401).json({ message: "Authorization Token Required" });
+    return res.status(401).json({ message: "Authorization Token Required" });
   }
 
   const token = authorization.split(" ")[1];
 
   // Verifies if the token is valid
   try {
-      const { _id } = jwt.verify(token, process.env.JWT_SECRET); // Extracting eMail from the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Check if the eMail exists in either FreelancerModel or ClientModel
-      const freelancer = await FreelancerModel.findOne({ _id });
-      const client = await ClientModel.findOne({ _id });
+    // Check if the user exists in either FreelancerModel or ClientModel
+    const freelancer = await FreelancerModel.findById(decoded._id);
+    const client = await ClientModel.findById(decoded._id);
 
-      // If eMail exists in either model, proceed to next middleware
-      if (freelancer || client) {
-          req.user = _id;
-          next();
-      } else {
-          return res.status(404).json({ message: "User not found" });
-      }
+    // If user exists in either model, attach user object to request and proceed to next middleware
+    if (freelancer || client) {
+      req.user = decoded; // Attach decoded token payload to request
+      next();
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
   } catch (err) {
-      res.status(404).json({ message: err.message });
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Token expired" });
+    }
+    if (err instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    res.status(500).json({ message: "Internal Server Error" });
   }
-}
+};
 
 module.exports = requireAuth;
