0d5f058f67c0697224944974418987206f7c77cc
vinsu <vencepuno.work@gmail.com>
2024-05-01 12:31:51 +0800

    ggs

--- backend/config/Paymongo.js
+++ backend/config/Paymongo.js
@@ -13,4 +13,17 @@ const createPayment = {
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
+    const getPayment = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        authorization: 'Basic c2tfdGVzdF9najNTOEFOVzcyUkY2aW1OWVZNQktKcnY6'
+      }
+    };
+    
+    fetch('https://api.paymongo.com/v1/links/id', getPayment)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
 module.exports = createPayment;
+module.exports = getPayment;
