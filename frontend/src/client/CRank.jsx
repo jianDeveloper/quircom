b17655c0b217fec66d01d79c34fa87f0e488e702
vinsu <vencepuno.work@gmail.com>
2024-06-07 00:28:18 +0800

    update

--- frontend/src/client/CRank.jsx
+++ frontend/src/client/CRank.jsx
@@ -107,15 +107,18 @@ function CRank() {
               unique projects, subsribe now. Join us and gain access to a world
               of opportunities tailored just for you!
             </p>
-            <div className="mx-auto w-1/2 relative rounded-tl-none rounded-lg shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-gradient-to-tr from-sky-600 to-sky-800 p-6 ">
-              <Link to={`/client/subscribe/${userId}`}>
+            <Link to={`/client/subscribe/${userId}`}>
+              <div className="mx-auto w-1/2 relative rounded-lg shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-gradient-to-tr from-sky-600 to-sky-800 p-6 ">
+                <button className="text-white font-bold text-2xl">
+                  Go to Subscription!
+                </button>
                 <img
-                  className="max-w-full h-auto rounded-lg transition-transform duration-300 shadow-lg hover:rotate-12"
+                  className="max-w-full h-auto rounded-lg transition-transform duration-300 hover:rotate-12"
                   src={BGSubs}
                   alt="Quircom Motion"
                 />
-              </Link>
-            </div>
+              </div>
+            </Link>
           </div>
         </div>
       ) : (
--- frontend/src/client/Marketcomponents/CConnect.jsx
+++ frontend/src/client/Marketcomponents/CConnect.jsx
@@ -98,15 +98,18 @@ function CConnect() {
                 your unique projects, subsribe now. Join us and gain access to a
                 world of opportunities tailored just for you!
               </p>
-              <div className="mx-auto w-1/3 relative rounded-tl-none rounded-lg shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-gradient-to-tr from-sky-600 to-sky-800 p-6 ">
-                <Link to={`/client/subscribe/${userId}`}>
+              <Link to={`/client/subscribe/${userId}`}>
+                <div className="mx-auto w-1/3 relative rounded-lg shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] bg-gradient-to-tr from-sky-600 to-sky-800 p-6 ">
+                  <button className="text-white font-bold text-2xl">
+                    Go to Subscription!
+                  </button>
                   <img
-                    className="max-w-full h-auto rounded-lg transition-transform duration-300 shadow-lg hover:rotate-12"
+                    className="max-w-full h-auto rounded-lg transition-transform duration-300 hover:rotate-12"
                     src={BGSubs}
                     alt="Quircom Motion"
                   />
-                </Link>
-              </div>
+                </div>
+              </Link>
             </div>
           </div>
         ) : (
@@ -148,61 +151,63 @@ function CConnect() {
               </p>
 
               <div className="mt-5 flex items-center">
-              <div className="flex items-center">
-  {userServices?.requestId?.length > 0 ? (
-    (() => {
-      const maxRating = 5;
-      // Filter out services with null or 0 feedback numbers
-      const filteredServices = userServices.requestId.filter(
-        (service) => service.feedbackNum > 0
-      );
-      const feedbackNums = filteredServices.map(
-        (service) => service.feedbackNum
-      );
-      const totalFeedbackNum = feedbackNums.reduce(
-        (acc, num) => acc + num,
-        0
-      );
-      const averageRating = feedbackNums.length
-        ? totalFeedbackNum / feedbackNums.length
-        : 0;
-      const filledStars = Math.floor(averageRating);
+                <div className="flex items-center">
+                  {userServices?.requestId?.length > 0
+                    ? (() => {
+                        const maxRating = 5;
+                        // Filter out services with null or 0 feedback numbers
+                        const filteredServices = userServices.requestId.filter(
+                          (service) => service.feedbackNum > 0
+                        );
+                        const feedbackNums = filteredServices.map(
+                          (service) => service.feedbackNum
+                        );
+                        const totalFeedbackNum = feedbackNums.reduce(
+                          (acc, num) => acc + num,
+                          0
+                        );
+                        const averageRating = feedbackNums.length
+                          ? totalFeedbackNum / feedbackNums.length
+                          : 0;
+                        const filledStars = Math.floor(averageRating);
 
-      return (
-        <>
-          {Array.from({ length: maxRating }).map((_, i) => (
-            <svg
-              key={i}
-              className={`block h-4 w-4 align-middle ${
-                i < filledStars
-                  ? "text-yellow-500"
-                  : i < Math.ceil(averageRating)
-                  ? "text-gray-200"
-                  : "text-gray-200"
-              }`}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-            </svg>
-          ))}
-        </>
-      );
-    })()
-  ) : null}
-  <p className="ml-2 text-sm font-medium text-gray-500">
-    {userServices && userServices.requestId && userServices.requestId.filter((service) => service.feedbackNum > 0).length
-      ? `${
-          userServices.requestId.filter(
-            (service) => service.feedbackNum > 0
-          ).length
-        } Reviews`
-      : "0 Reviews"}
-  </p>
-</div>
-
-
+                        return (
+                          <>
+                            {Array.from({ length: maxRating }).map((_, i) => (
+                              <svg
+                                key={i}
+                                className={`block h-4 w-4 align-middle ${
+                                  i < filledStars
+                                    ? "text-yellow-500"
+                                    : i < Math.ceil(averageRating)
+                                    ? "text-gray-200"
+                                    : "text-gray-200"
+                                }`}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                              </svg>
+                            ))}
+                          </>
+                        );
+                      })()
+                    : null}
+                  <p className="ml-2 text-sm font-medium text-gray-500">
+                    {userServices &&
+                    userServices.requestId &&
+                    userServices.requestId.filter(
+                      (service) => service.feedbackNum > 0
+                    ).length
+                      ? `${
+                          userServices.requestId.filter(
+                            (service) => service.feedbackNum > 0
+                          ).length
+                        } Reviews`
+                      : "0 Reviews"}
+                  </p>
+                </div>
               </div>
 
               <div className="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0 text-[#1D5B79]">
